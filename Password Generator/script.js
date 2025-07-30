8714cce4fe4fce8540fe72b0320656e0b2ae9880
safran6ix <mohammedsafran6ix@gmail.com>
2025-07-31 01:21:37 +0530

    Modify

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -1,8 +1,10 @@
 var password = document.getElementById("password");
+
+//Generate password function
 function genPassword() {
     var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var passLenght = 15;
-    var password = "";
+    var password = " ";
     for (var i = 0; i < passLenght; i++) {
         var randomNumber = Math.floor(Math.random() * chars.length);
         password += chars.substring(randomNumber, randomNumber + 1);
@@ -10,6 +12,7 @@ function genPassword() {
     document.getElementById("password").value = password;
 }
 
+//copy password function 
 function copyPassword() {
     var copyText = document.getElementById("password");
     copyText.select();
