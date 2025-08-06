006f20234cb5acb53d7464b1c56b42d0a316f7b7
safran6ix <mohammedsafran6ix@gmail.com>
2025-08-06 20:16:49 +0530

    Adding DOM Elements

--- Camera implementation/script.js
+++ Camera implementation/script.js
@@ -1,1 +1,7 @@
-const 
+// DOM elements
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+const captureButton = document.getElementById('captureButton');
+const photosContainer = document.getElementById('photos');
