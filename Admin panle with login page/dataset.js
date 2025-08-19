2d8506881bd7add75cb0baf7b41b23db94f1f962
safran6ix <mohammedsafran6ix@gmail.com>
2025-08-20 00:26:16 +0530

    Adding data set js

---
+++ Admin panle with login page/dataset.js
@@ -0,0 +1,19 @@
+// Sample user database
+const users = {
+    "codify": {
+        username: "codify",
+        password: "12345",
+        name: "Codify6ix",
+        accountNumber: "123-456-789",
+        balance: 1000,
+        address: "253f, lorem the hype, deth ruin diese."
+    },
+    "user2": {
+        username: "user2",
+        password: "123456",
+        name: "Jane Smith",
+        accountNumber: "987-654-321",
+        balance: 2000,
+        address: "Empty"
+    }
+};
