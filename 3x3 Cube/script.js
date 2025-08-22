fe22e76b551724399d3d754134eff54294fc94d8
safran6ix <mohammedsafran6ix@gmail.com>
2025-08-22 18:55:14 +0530

    Selecting Dom

--- 3x3 Cube/script.js
+++ 3x3 Cube/script.js
@@ -1,1 +1,8 @@
-const
+document.addEventListener('DOMContentLoaded', function () {
+    const cube = document.querySelector('.cube');
+    const rotateXBtn = document.getElementById('rotate-x');
+    const rotateYBtn = document.getElementById('rotate-y');
+    const rotateZBtn = document.getElementById('rotate-z');
+    const resetBtn = document.getElementById('reset');
+    
+});
