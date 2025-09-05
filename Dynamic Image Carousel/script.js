2608153e68a8bf99e27f526111697505dc2c46c6
safran6ix <mohammedsafran6ix@gmail.com>
2025-09-05 23:53:25 +0530

    Selecting Variables

---
+++ Dynamic Image Carousel/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Elements
+    const carousel = document.querySelector('.carousel');
+    const items = document.querySelectorAll('.carousel-item');
+    const prevBtn = document.querySelector('.prev');
+    const nextBtn = document.querySelector('.next');
+    const indicatorsContainer = document.querySelector('.carousel-indicators');
+    const autoplayBtn = document.querySelector('.autoplay-btn');
+
+})
